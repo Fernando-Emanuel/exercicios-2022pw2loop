f38204cba8eb3529c060e898cd74801e07a2be5a
Fernando-Emanuel <fernando2004emanuel@gmail.com>
2022-03-23 17:51:29 +0000

    Main

--- Exercicio1.java
+++ Exercicio1.java
@@ -1,15 +1,19 @@
 public class Exercicio1 {
     public static void main(String[] args) {
-        //Percorre os número de 1 á 500
+        //Armazena a soma
         int soma = 0;
+        // Percorre os números de 1 à 500
         for(int num = 1;num <= 500;num++) {
-            //Verificar se o número é ímpar
+            // Verificar se o número é ímpar
             if((num % 2) != 0) {
+                // Verifica se o número é multiplo de 3
                 if((num % 3) == 0) {
-                soma += num;
+                    // System.out.println(num);
+                    // soma += num;
+                    soma = soma + num;
+                }
             }
         }
-      }
-    }
+        System.out.println(soma);    
+    }   
 }
-System.out.println(soma);
---
+++ Exercicio3.java
@@ -0,0 +1,7 @@
+public class Exercicio3 {
+    public static void main(String[] args) {
+        int vetor[] = {15, 20, 55, 10, 85, 36, 77, 56, 66, 25};
+        int menorValor = vetor[0];
+        int maiorValor = vetor[0];
+    }
+}
